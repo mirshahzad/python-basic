fbd58c661f3534944b64362f34c203e9dca04f14
mirshahzad <meershahzad@hotmail.com>
2020-05-01 13:33:37 +0200

    exercise_famous_quote

---
+++ exercise_famouse_quote.py
@@ -0,0 +1,33 @@
+#exercise 2.3
+person_name = "Mir"
+print(f"\"Hello {person_name} would you like to learn some Python today?\"")
+
+#exercise 2.4
+name = "Mir"
+print(name.title())
+print(name.upper(), name.lower())
+
+
+#exercise 2.5
+quote = "A lot of people are afraid to tell the truth, to say no. Thats where toughness comes into play. Toughness is not being a bully. Its having backbone."
+print(f"Robert T. Kiyosaki said \"{quote}\"")                                                                                                                                        
+
+#exercise 2.6
+famous_person = "Robert T. Kiyosaki"
+message = famous_person.title()
+print(message)
+
+#exercise 2.7
+name = "mir"
+print("\nname \tname \nname")
+
+#exercise 2.8
+print(3+5)
+print(4*2)
+print(10-2)
+print(24/3)
+
+#exercise 2.9
+favorite_number = 4
+message = (f"This is my favorite number {favorite_number}")
+print (message)
