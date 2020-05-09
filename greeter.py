01ab35819b5f707d9572bf22b1e1a777af6de4e5
mirshahzad <meershahzad@hotmail.com>
2020-05-09 14:37:39 +0200

    greeter

---
+++ greeter.py
@@ -0,0 +1,8 @@
+name = input("Please enter you name: ")
+print(f"\nHello, {name}!")
+
+prompt = "If you tell us who you are, we can personalize the message you see."
+prompt += "\nWhat is you first name? "
+
+name = input(prompt)
+print(f"\nHey, {name}!")
