0fdb7b02b3cd8785c2f77dd58182cdb9a07fea6d
mirshahzad <meershahzad@hotmail.com>
2020-06-09 10:10:09 +0200

    toppings

---
+++ toppings.py
@@ -0,0 +1,27 @@
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+	print("Hold the anchovies!")
+
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+	print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+	print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+	print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+	if requested_topping == 'green peppers':
+		print("Sorry, we are out of green peppers right now.")
+	else:
+		print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
